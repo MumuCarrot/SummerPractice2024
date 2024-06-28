24fa36d08f6a9f440e7fa20595a24da15b44d7b6
RARadchenko <r.a.radchenko@student.csn.khai.edu>
2024-06-28 11:14:43 +0300

    Added files. Build successful.

---
+++ gitpush.sh
@@ -0,0 +1,5 @@
+git config --global user.email "r.a.radchenko@student.csn.khai.edu"
+git config --global user.name "RARadchenko"
+git add .
+git commit -m "Added files. Build successful."
+git push origin HEAD:master --force
